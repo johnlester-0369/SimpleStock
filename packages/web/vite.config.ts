d64f862b7656c28f21becda5299d15f2124d95a3
John Lester <johnlester.0369@gmail.com>
2026-01-15 16:29:03 +0800

    Remove base configuration from Vite config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -6,7 +6,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
